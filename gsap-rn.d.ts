5d58d23d0b69fe2d36fbf7b5ada3a6efcc347d3c
Tufik Chediak <tufik2@gmail.com>
2023-05-01 10:22:15 -0400

    Generate type declarations

---
+++ gsap-rn.d.ts
@@ -0,0 +1,14 @@
+export class AutoKillTweens {
+    static kill(tweens: any): void;
+    static tweensOf(tweens: any): void;
+    constructor(props: any);
+    componentWillUnmount(): void;
+    render(): any;
+}
+export namespace AutoKillTweens {
+    namespace defaultProps {
+        const tweens: any;
+    }
+}
+export { gsap, TweenMax, TweenLite, TimelineMax, TimelineLite, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ };
+//# sourceMappingURL=gsap-rn.d.ts.map
